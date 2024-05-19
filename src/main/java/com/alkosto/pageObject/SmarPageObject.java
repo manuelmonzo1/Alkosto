945025387f558e3ec99ff754b7051f32b996515f
Manuel Monzon <monzonmanuelguillermo@gmail.com>
2024-05-19 17:40:45 -0500

    Alkosto Test Smar1

--- src/main/java/com/alkosto/pageObject/SmarPageObject.java
+++ src/main/java/com/alkosto/pageObject/SmarPageObject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class SmarPageObject extends PageObject {
 
-    private By linkSmat = By.xpath("//h3[@data-id=\"6973230611343\"]");
+    private By linkSmat = By.xpath("//h3[@data-id='6973230611343']");
 
     public By getLinkSmat() {
         return linkSmat;
